2f86d7232d953b860467198a9324ec2ab742af2e
Mike <a8578155@gmail.com>
2024-07-11 22:38:22 +0800

    up2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 27017;
+const port = 3000;
 
 // 解析 application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
